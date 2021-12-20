fb19f266610870af0ca782178bf4e4dc34ab9ded
Kireeva <kireeva.en@defo.ru>
2021-12-20 15:46:47 +0300

    inishial

---
+++ js/script.js
@@ -0,0 +1,24 @@
+"use strict"
+
+// -- Определяем на каком устройстве открыта страница
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows());
+    }
+};
