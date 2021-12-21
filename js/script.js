cb310afbd09687ee95e530b2abfd5348ee8672f1
Kireeva <kireeva.en@defo.ru>
2021-12-21 17:54:07 +0300

    закончено

--- js/script.js
+++ js/script.js
@@ -36,10 +36,23 @@ if (isMobile.any()) {
                 menuArrow.parentElement.classList.toggle('_active');
             });
         }
-    }else{
-        document.body.classList.add('_pc');
     }
+}else{
+    document.body.classList.add('_pc');
 };
+
+
+// -- меню бургер
+const iconMenu = document.querySelector('.menu__icon');
+const menuBody = document.querySelector('.menu__body');
+
+if(iconMenu){
+    iconMenu.addEventListener('click', function(e){
+        document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('_active');
+        menuBody.classList.toggle('_active');
+    });
+}
 
 
 // прокрутка при клике на меню
@@ -55,11 +68,19 @@ if (menuLinks.length > 0){
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight;
 
+            if(iconMenu.classList.contains('_active')){
+                document.body.classList.remove('_lock');
+                iconMenu.classList.remove('_active');
+                menuBody.classList.remove('_active');
+            }
+
             window.scrollTo({
                 top: gotoBlockValue,
-                behavior: "smooth"
+                behavior: "smooth",
             });
             e.preventDefault();
         }
     }
 }
+
+
